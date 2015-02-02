0637c4c33f471734964d434c169e736b8a89709c
Ravi Sadineni <rsadineni@compute-3-28.local>
2015-02-01 19:20:37 -0500

    removing merge conflicts

--- bin/sbush/sbush.h
+++ bin/sbush/sbush.h
@@ -14,9 +14,3 @@ struct job
 };
 
 int make_job(struct job* cmd_list,char * cmdline);
-<<<<<<< HEAD
-=======
-int getcountofchar(char* string, char character);
-int getfirstindex(char* string,char character);
-char** strtokenize(char* string, char character);
->>>>>>> 8feccc6f0c898c731bc550600f1442f77e0ccc15
