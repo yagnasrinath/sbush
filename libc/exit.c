8f14286fda744bf4dadfbd38541ea1b883364df2
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-02-09 01:40:07 -0500

    calling exit

--- libc/exit.c
+++ libc/exit.c
@@ -3,9 +3,12 @@
 #include <stdarg.h>
 
 void exit(int status) {
-	 asm("movl $1,%%rax;"         /* SYS_exit is 1 */
-	     "xorl %%rbx,%%rbx;"      /* Argument is in ebx, it is 0 */
-	     "int  $0x80"            /* Enter kernel mode */
+	 asm("movw $1,%%rax;"
+			 "xorw %%rbx,%%rbx;"
+			 "int  $0x80;"
+			 :
+			 :
+			 :
 	  );
 
 }
