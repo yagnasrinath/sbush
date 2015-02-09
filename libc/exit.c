62f243ba2a5be317d33016c5a38abeb7f169e1b1
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-02-09 04:41:28 -0500

    syscalls defined

--- libc/exit.c
+++ libc/exit.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include<syscall.h>
 void exit(int status) {
-	 asm("movw $1,%%rax;"
-			 "int  $0x80;"
-			 :
-			 :"d"(status)
-			 :
-	  );
-
+	syscall_1((uint64_t)SYS_exit,(uint64_t)status);
 }
 
--- libc/write.c
+++ libc/write.c
@@ -8,14 +8,8 @@
 
 
 ssize_t write(int fd, const void *buf, size_t count){
-	ssize_t ret;
-	asm volatile
-	(
-			"syscall"
-			: "=a" (ret)
-			: "0"(SYS_write), "D"(fd), "S"(buf), "d"(count)
-			: "cc", "rcx", "r11", "memory"
-	);
-	return ret;
+
+
+	return syscall_3((uint64_t)SYS_write,(uint64_t)fd,(uint64_t)buf, (uint64_t)count);
 }
 
