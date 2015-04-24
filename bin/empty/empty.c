f643a2b42c96ddff26370252c94cc457c531a9b4
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-04-24 03:08:38 -0400

    stable waitpid

--- bin/empty/empty.c
+++ bin/empty/empty.c
@@ -16,21 +16,21 @@ int main(int argc, char* argv[], char* envp[]) {
 	pid = fork();
 	if(pid == 0) {
 		printf("In child process \n");
-		/*int *b ;
+		int *b ;
 		b = (int*)malloc(sizeof(int));
 		 *b = 3;
 		printf("In child process %d\n",*b);
 		sleep(3);
-		printf("In child process after sleep \n");*/
-		//printf("In child process %d\n",getpid());
-		//printf("In child process %d\n",getppid());
+		printf("In child process after sleep \n");
+		printf("In child process %d\n",getpid());
+		printf("In child process %d\n",getppid());
 	}
 	else {
 		printf("Pid of parent process is %d \n", pid);
-		//printf("In parent process %d\n",getpid());
-		//printf("In parent process %d\n",getppid());
-		//int wait = waitpid(37,0,0);
-		//printf("The value returned by waitpid is %d\n",wait);
+		printf("In parent process %d\n",getpid());
+		printf("In parent process %d\n",getppid());
+		int wait = waitpid(37,0,0);
+		printf("The value returned by waitpid is %d\n",wait);
 		//int *b = 4;
 		//printf("In parent process %d\n",b);
 	}
--- sys/SyscallManagement/syscall.c
+++ sys/SyscallManagement/syscall.c
@@ -17,6 +17,36 @@ struct timespec{
 };
 typedef struct timespec timespec;
 
+ enum registers {
+	 ZERO,
+	 SS1,
+	 SS2,
+	 STK_TOP,
+	 FLAGS_PROC,
+	 CS,
+	 ENTRY,
+	 LOCAL_VAR,
+	 RDI,
+	 RAX,
+	 RBX,
+	 RCX,
+	 RDX,
+	 RBP,
+	 RSI,
+	 R8,
+	 R9,
+	 R10,
+	 R11,
+	 R12,
+	 R13,
+	 R14,
+	 R15
+
+
+};
+
+
+
 #define POPA \
 		__asm__ __volatile__(\
 				"popq %r15;"\
@@ -69,15 +99,15 @@ void fork()  {
 	kmemset(child_task->kstack, 0 , PAGE_SIZE);
 	kprintf("pml4t of child is %p \n",child_task->virtual_addr_space->pml4_t );
 	kprintf("pml4t of parent is %p \n",curr_task->virtual_addr_space->pml4_t );
-	schedule_process(child_task, curr_task->kstack[KSTACK_SIZE-4], curr_task->kstack[KSTACK_SIZE-7]);
-	curr_task->kstack[KSTACK_SIZE-10] = child_task->pid;
+	schedule_process(child_task, curr_task->kstack[KSTACK_SIZE-STK_TOP], curr_task->kstack[KSTACK_SIZE-ENTRY]);
+	curr_task->kstack[KSTACK_SIZE-RAX] = child_task->pid;
 }
 
 
 void sys_write(){
 	task_struct * curr_task = get_curr_task();
-	uint64_t fd = curr_task->kstack[KSTACK_SIZE-9];
-	uint64_t addr = curr_task->kstack[KSTACK_SIZE-15];
+	uint64_t fd = curr_task->kstack[KSTACK_SIZE-RDI];
+	uint64_t addr = curr_task->kstack[KSTACK_SIZE-RSI];
 	//uint64_t len = curr_task->kstack[KSTACK_SIZE-13];
 	//kprintf("File Descriptor is %d\n",fd);
 	if(fd == 1 || fd == 2){
@@ -90,7 +120,7 @@ void sys_write(){
 
 void sys_brk() {
 	task_struct * curr_task = get_curr_task();
-	uint64_t addr = curr_task->kstack[KSTACK_SIZE-9];
+	uint64_t addr = curr_task->kstack[KSTACK_SIZE-RDI];
 	kprintf("passed addr is %p \n", addr);
 	uint64_t max_possile_addr = USR_STK_TOP - USR_STK_SIZE;
 	vma_struct* curr_vmaList = curr_task->virtual_addr_space->vmaList;
@@ -105,34 +135,34 @@ void sys_brk() {
 		panic("SYs_break : NO HEAP VMA");
 	}
 	if(addr ==0) {
-		curr_task->kstack[KSTACK_SIZE-10] = curr_vma->vm_area_end;
+		curr_task->kstack[KSTACK_SIZE-RAX] = curr_vma->vm_area_end;
 	}
 	else if (addr < curr_vma->vm_area_end || addr >= max_possile_addr) {
-		curr_task->kstack[KSTACK_SIZE-10]  = -1;
+		curr_task->kstack[KSTACK_SIZE-RAX]  = -1;
 	}
 	else {
-		curr_task->kstack[KSTACK_SIZE-10] = addr;
+		curr_task->kstack[KSTACK_SIZE-RAX] = addr;
 		if(addr%PAGE_SIZE != 0) {
 			addr = PAGE_ALIGN(addr) + PAGE_SIZE;
 		}
 		curr_vma ->vm_area_end = addr;
 		//curr_task->kstack[KSTACK_SIZE-10] = curr_vma ->vm_area_end;
 	}
-	kprintf("return addr is %p \n", curr_task->kstack[KSTACK_SIZE-10]);
+	kprintf("return addr is %p \n", curr_task->kstack[KSTACK_SIZE-RAX]);
 }
 
 
 void sleep() {
 	task_struct * curr_task = get_curr_task();
-	uint64_t* sleep_for = (uint64_t*)curr_task->kstack[KSTACK_SIZE-9];
-	uint64_t* rem_time = (uint64_t*)curr_task->kstack[KSTACK_SIZE-15];
+	uint64_t* sleep_for = (uint64_t*)curr_task->kstack[KSTACK_SIZE-RDI];
+	uint64_t* rem_time = (uint64_t*)curr_task->kstack[KSTACK_SIZE-RSI];
 	timespec* timeSpec = (timespec*)sleep_for ;
 	curr_task->state = SLEEP;
 	curr_task->sleep_for = timeSpec->tv_sec * 100;
 	timeSpec = (timespec*)rem_time ;
 	timeSpec->tv_sec = 0;
 	timeSpec->tv_nsec = 0;
-	curr_task->kstack[KSTACK_SIZE-10] = 0;
+	curr_task->kstack[KSTACK_SIZE-RAX] = 0;
 	__asm__ __volatile__("int $32;");
 }
 
@@ -141,35 +171,35 @@ void sleep() {
 void alarm() {
 	task_struct * curr_task = get_curr_task();
 	curr_task->state = SLEEP;
-	curr_task->sleep_for = curr_task->kstack[KSTACK_SIZE-9] * 100;
-	curr_task->kstack[KSTACK_SIZE-10] = 0;
+	curr_task->sleep_for = curr_task->kstack[KSTACK_SIZE-RDI] * 100;
+	curr_task->kstack[KSTACK_SIZE-RAX] = 0;
 	__asm__ __volatile__("int $32;");
 }
 
 
 void getpid() {
 	task_struct * curr_task = get_curr_task();
-	curr_task->kstack[KSTACK_SIZE-10] = curr_task->pid;
+	curr_task->kstack[KSTACK_SIZE-RAX] = curr_task->pid;
 }
 
 
 void getppid() {
 	task_struct * curr_task = get_curr_task();
-	curr_task->kstack[KSTACK_SIZE-10] = curr_task->ppid;
+	curr_task->kstack[KSTACK_SIZE-RAX] = curr_task->ppid;
 }
 
 void waitpid(){
 	task_struct * curr_task = get_curr_task();
 	task_struct *child_head = curr_task->children_head;
-	int wait_for = curr_task->kstack[KSTACK_SIZE-9];
+	int wait_for = curr_task->kstack[KSTACK_SIZE-RDI];
 	if(curr_task->children_head == NULL){
-		curr_task->kstack[KSTACK_SIZE-10] = -1;
+		curr_task->kstack[KSTACK_SIZE-RAX] = -1;
 		return;
 	}
 	if(wait_for == -1){
 		while(child_head != NULL){
 			if(child_head->state == ZOMBIE){
-				curr_task->kstack[KSTACK_SIZE-10] = child_head->pid;
+				curr_task->kstack[KSTACK_SIZE-RAX] = child_head->pid;
 				return;
 			}
 			child_head = child_head->siblings;
@@ -189,7 +219,7 @@ void waitpid(){
 		if(child_head != NULL){
 			if(child_head ->state == ZOMBIE) {
 				child_head ->state  = EXIT;
-				curr_task->kstack[KSTACK_SIZE-10] = child_head->pid;
+				curr_task->kstack[KSTACK_SIZE-RAX] = child_head->pid;
 				return;
 			}
 			curr_task->wait_pid = wait_for;
@@ -197,7 +227,7 @@ void waitpid(){
 			__asm__ __volatile__("int $32;");
 		}
 		else{
-			curr_task->kstack[KSTACK_SIZE-10] = -1;
+			curr_task->kstack[KSTACK_SIZE-RAX] = -1;
 			return;
 		}
 	}
@@ -216,11 +246,12 @@ void exit(){
 void handle_syscall() {
 	PUSHA;
 	task_struct* curr_task = get_curr_task();
-	if(curr_task->kstack[KSTACK_SIZE-10] <0 || curr_task->kstack[KSTACK_SIZE-10] >19) {
-		kprintf("Invalid syscall number %p\n",curr_task->kstack[KSTACK_SIZE-10]);
+	//kprintf("syscall number %p\n",curr_task->kstack[KSTACK_SIZE-RAX]);
+	if(curr_task->kstack[KSTACK_SIZE-RAX] <0 || curr_task->kstack[KSTACK_SIZE-RAX] >19) {
+		kprintf("Invalid syscall number %p\n",curr_task->kstack[KSTACK_SIZE-RAX]);
 	}
 	else {
-		switch(curr_task->kstack[KSTACK_SIZE-10]) {
+		switch(curr_task->kstack[KSTACK_SIZE-RAX]) {
 		case 12 : alarm();break;
 		case 11 : sleep(); break;
 		case 10 : waitpid();break;
