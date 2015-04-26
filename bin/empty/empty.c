4f81b26368a5ea3ee105f8a4a60a303c811a39cb
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-04-26 19:07:29 -0400

    empty projec

--- bin/empty/empty.c
+++ bin/empty/empty.c
@@ -14,13 +14,8 @@
 int main(int argc, char* argv[], char* envp[]) {
 	//int pid = -1;
 	int i=0;
-	for(i =0;i<13;i++){
+	printf("in process loop %d\n",i);
 
-		int pid = fork();
-		printf("in loop %d\n",pid);
-			//printf("pid is %d\n",pid);
-
-	}
 
 
 	//while(1);
