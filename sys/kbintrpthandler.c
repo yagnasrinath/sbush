192d18b5f287673f1d7999da52407e860332a95b
ravichandrasadineni <ravichandrasadineni@gmail.com>
2015-05-06 16:34:07 -0400

    ctrl +c handling

--- sys/kbintrpthandler.c
+++ sys/kbintrpthandler.c
@@ -1,10 +1,3 @@
-/*
- * kbintrpthandler.c
- *
- *  Created on: Mar 1, 2015
- *      Author: ravichandrasadineni
- */
-
 #include <sys/kbsctochar.h>
 #include <sys/scrn.h>
 #include <sys/system.h>
@@ -16,6 +9,8 @@ static  volatile  uint16_t LEFT_SHIFT_PRESS = 1;
 static  volatile  uint16_t LEFT_SHIFT_RELEASE = ~(1);
 static  volatile  uint16_t RIGHT_SHIFT_PRESS = 2;
 static  volatile  uint16_t RIGHT_SHIFT_RELEASE = ~(2);
+static  volatile  uint16_t CTRL_PRESS = 4;
+static  volatile  uint16_t CTRL_RELEASE = ~(4);
 static volatile uint16_t flag =0;
 static volatile uint64_t last_addr;
 static volatile uint64_t counter;
@@ -27,8 +22,8 @@ static volatile char buf[1024];
 void init_keyboard()
 {
 	special_keys = 0;
-    last_addr = 0;
-    flag = counter = 0;
+	last_addr = 0;
+	flag = counter = 0;
 }
 
 int gets(uint64_t addr, uint64_t length)
@@ -97,35 +92,47 @@ void  kb_intrpt_handler( struct isr_nrm_regs  stack) {
 	else if (b == (SC_RIGHTSHIFT  |0X80)) {
 		special_keys = special_keys & RIGHT_SHIFT_RELEASE;
 	}
+	else if(b == SC_CTRL ){
+		special_keys = special_keys	| CTRL_PRESS;
+	}
+	else if(b == (SC_CTRL | 0x80)){
+		special_keys = special_keys & CTRL_RELEASE;
+	}
 	else if( b < 0X80){
 		BOOL is_shift_pressed = (special_keys && LEFT_SHIFT_PRESS) ||
 				(special_keys && RIGHT_SHIFT_PRESS);
-		uint64_t val = get_asci_for_sc(b,is_shift_pressed);
-		if (flag == 1) {
-			if (val == '\n') {
-				buf[counter++] = '\n';
-				buf[counter++] = '\0';
-				putch(val);
-				flag = 0;
-			}
-			else if (val == '\b') {
-				if (get_current_addr() > last_addr) {
+		BOOL is_ctrl_pressed = (special_keys && CTRL_PRESS);
+		if(is_ctrl_pressed && (b == SC_C || b == SC_D)){
+				counter = 0;
+				flag= 0;
+		}
+		else{
+			uint64_t val = get_asci_for_sc(b,is_shift_pressed);
+			//kprintf("in else %c\n",b);
+			if (flag == 1) {
+				if (val == '\n') {
+					buf[counter++] = '\n';
+					buf[counter++] = '\0';
 					putch(val);
-					counter--;
-				}
-			} else {
-				if(valid_ascii(val))
-				{
-					buf[counter] = val;
-					counter += 1;
-				}
-				putch(val);
-				if(counter == num_char_to_read) {
 					flag = 0;
+				}
+				else if (val == '\b') {
+					if (get_current_addr() > last_addr) {
+						putch(val);
+						counter--;
+					}
+				} else {
+					if(valid_ascii(val))
+					{
+						buf[counter] = val;
+						counter += 1;
+					}
+					putch(val);
+					if(counter == num_char_to_read) {
+						flag = 0;
+					}
 				}
 			}
 		}
 	}
 }
-
-
